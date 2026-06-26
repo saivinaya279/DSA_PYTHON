17b9d4f57957c7313ef17edc0788eb3a838a7ca2
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-26 20:00:00 +0530

    commit

--- ARRAY2/matrix.py
+++ ARRAY2/matrix.py
@@ -17,3 +17,8 @@ for j in range(c):
         print(n[i][j],end =" ")
         print()
 
+target=int(input())
+for krow in range(len(n)):
+    for ele in range(len(n[krow])):
+        if ele==target:
+            print(krow,ele)
